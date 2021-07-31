04e5c569ca9ae88d4c4a03137db9f11dc7d02f9d
Veras <alessandro.veras@italtel.com>
2021-07-31 15:28:57 -0300

    recovered broken issue.

--- src/main/java/br/edu/infnet/bootalunoapp/repository/TurmaRepository.java
+++ src/main/java/br/edu/infnet/bootalunoapp/repository/TurmaRepository.java
@@ -3,7 +3,7 @@ package br.edu.infnet.bootalunoapp.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//import br.edu.infnet.bootalunoapp.modelo.Turma;
+import br.edu.infnet.bootalunoapp.modelo.Turma;
 
 @Repository
 public interface TurmaRepository extends JpaRepository<Turma, Long> {
